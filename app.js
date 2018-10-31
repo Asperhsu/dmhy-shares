f2c9ac51ceefa5a0d72c9eb6c6232eaffad1c0a6
Asper <asperwon@gmail.com>
2018-11-01 00:08:46 +0800

    cleanup

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
 
 
-// console.log(programs)
-var dmhyUrl = "http://share.dmhy.org/topics/list?keyword=";
+var todayDayNo = (new Date).getDay();
 var app = new Vue({
     el: '#app',
 
     data: {
-        programs: programs,
+        allPrograms: programs,
         dayNames: ['週日', '週一', '週二', '週三', '週四', '週五', '週六'],
         dmhyUrl: "http://share.dmhy.org/topics/list?keyword=",
         keyword: null,
+        dayNo: todayDayNo,
     },
 
     computed: {
-        weekNo () {
-            return (new Date).getDay();
-        },
         iframwSrc () {
             return this.dmhyUrl + this.keyword;
+        },
+        dayPrograms () {
+            return this.allPrograms[this.dayNo];
         }
     },
-
-    methods: {
-        getDayPrograms(dayNo) {
-            return this.programs[dayNo];
-        }
-    }
 })
 
 Split(['#top', '#down'], {
-    sizes: [25, 75],
+    sizes: [20, 80],
     direction: 'vertical'
 });
